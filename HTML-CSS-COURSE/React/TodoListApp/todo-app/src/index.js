e2b6fbd3207f9126c91b9c6735e5f82acde3ac13
Anjali598 <tanjali.devi3@gmail.com>
2025-02-05 15:08:42 +0530

    updated todo app index.js

--- HTML-CSS-COURSE/React/TodoListApp/todo-app/src/index.js
+++ HTML-CSS-COURSE/React/TodoListApp/todo-app/src/index.js
@@ -4,24 +4,24 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Todo from "./Todo";
-import { HashRouter, Routes, Route } from "react-router-dom";
+//import { HashRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//     <Todo />
-//   </React.StrictMode>
-// );
-
 root.render(
-  <HashRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/todo" element={<Todo />} />
-    </Routes>
-  </HashRouter>
+  <React.StrictMode>
+    <App />
+    <Todo />
+  </React.StrictMode>
 );
+
+// root.render(
+//   <HashRouter>
+//     <Routes>
+//       <Route path="/" element={<App />} />
+//       <Route path="/todo" element={<Todo />} />
+//     </Routes>
+//   </HashRouter>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
